e8c60bb642ca67ffc14b901325a10a1fc39ae800
Dylan Egan <me@dylanegan.com>
2009-08-02 14:46:17 +1000

    Basic spec for Parser base class.

--- lib/at_the_movies/parser.rb
+++ lib/at_the_movies/parser.rb
@@ -1,5 +1,18 @@
 module AtTheMovies
   class Parser
+    attr_reader :page
 
+    def self.parse(page)
+      parser = new(page)
+      parser.parse
+    end
+
+    def initialize(page)
+      @page = page
+    end
+
+    def parse
+      raise NotImplementedError, "where am I?"
+    end
   end
 end
--- spec/at_the_movies/parser_spec.rb
+++ spec/at_the_movies/parser_spec.rb
@@ -1,4 +1,8 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe AtTheMovies::Parser do
+  it "should act as a base class" do
+    parser = AtTheMovies::Parser.new(nil)
+    lambda { parser.parse }.should raise_error(NotImplementedError)
+  end
 end
