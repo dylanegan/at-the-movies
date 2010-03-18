ddf883654237a3b82eca908721ff680c747178c2
Dylan Egan <me@dylanegan.com>
2010-03-18 17:20:24 +1100

    Ugh ugly hack to get around their new SHITE HTML!

--- lib/at_the_movies/parsers.rb
+++ lib/at_the_movies/parsers.rb
@@ -15,7 +15,15 @@ module AtTheMovies
         raise AtTheMovies::ParserError,
           "Failed to parse #{url}. #{e.message}"
       end
-      type = page.search('meta[@name="ABC-Author"]').first['content']
+      begin
+        if score = page.search('p[@class="score"]')
+          type = "review" if score.text[/Margaret|David/]
+        else
+          type = page.search('meta[@name="ABC-Author"]').first['content']
+        end
+      rescue
+        type = "n/a"
+      end
       return if options[:only] and type != options[:only]
       parser = MAP[type] || raise(ArgumentError, "Couldn't find a Parser class to parse a #{type.inspect} page")
       parser.parse(page)
